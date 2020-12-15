e4e867609c5ad4431f8353f00bf6e21ef6cc864e
Туран <t.nurtayev@almatydc.local>
2020-12-15 10:45:33 +0600

    proccessing:css animation

--- src/components/CardPanel.js
+++ src/components/CardPanel.js
@@ -1,86 +1,46 @@
-import React, { useEffect, useState, useMemo } from 'react'
+import React, { useMemo } from 'react'
 import { LeftCircleFilled, RightCircleFilled } from '@ant-design/icons'
 
 import Card from './Card'
 
 const CardPanel = ({ blocks, district }) => {
-  const [classname, setClassname] = useState('')
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    return () => {
-      setClassname('')
-      setCount(0)
-    }
-  }, [])
-
   const slides = useMemo(() => {
     let arr = fillCard(blocks, district)
     arr = doubleCard(arr, district)
-    return fillSlide(arr, district, classname).map((i, index) => (
+    return fillSlide(arr, district).map((i, index) => (
       <div
-        className={`cardpanel_slide ${classname}`}
+        className={`cardpanel_slide`}
         key={`cardpanel-slide-${district}-${index + 1}`}
       >
         {i}
+        <input
+          type='radio'
+          name='radio-left'
+          id={`slide-control-${index}`}
+          className={'left'}
+        />
+        <a href='#st-panel-2' className={'left'}>
+          <LeftCircleFilled className={'icon'} />
+        </a>
+
+        <input
+          type='radio'
+          name='radio-right'
+          id={`slide-control-${index === arr.length - 1 ? 0 : index + 1}`}
+          className={'right'}
+        />
+        <a href='#st-panel-2' className={'right'}>
+          <RightCircleFilled className={'icon'} />
+        </a>
       </div>
     ))
-  }, [blocks, district, classname])
-
-  const prev = (count) => {
-    if (count - 1 >= 0) {
-      setCount(count - 1)
-      setClassname(`prev${count - 1}`)
-    } else {
-      if (blocks.length > 12) {
-        setCount(2)
-        setClassname(`next2`)
-      }
-      if (blocks.length > 6 && blocks.length <= 12) {
-        setCount(1)
-        setClassname('next1')
-      }
-    }
-  }
-
-  const next = (count) => {
-    if (blocks.length > 12) {
-      if (count < 2) {
-        setCount(count + 1)
-        setClassname(`next${count + 1}`)
-      } else {
-        setCount(0)
-        setClassname('prev0')
-      }
-    }
-    if (blocks.length > 6 && blocks.length <= 12) {
-      if (count + 1 < 2) {
-        setCount(count + 1)
-        setClassname(`next${count + 1}`)
-      } else {
-        setCount(0)
-        setClassname('prev0')
-      }
-    }
-  }
+  }, [blocks, district])
 
   return (
     <div className={'cardpanel'}>
       {slides.map((s) => s)}
-      <LeftCircleFilled
-        className={'nav_icon left'}
-        onClick={() => prev(count)}
-        // disabled={count === 0}
-      />
-      <RightCircleFilled
-        className={'nav_icon right'}
-        onClick={() => next(count)}
-        // disabled={
-        //   (blocks.length > 12 && count === 2) ||
-        //   blocks.length < 7 ||
-        //   (blocks.length < 13 && count === 1)
-        // }
-      />
+      {/* <LeftCircleFilled className={'nav_icon left'} />
+      <RightCircleFilled className={'nav_icon right'} /> */}
     </div>
   )
 }
@@ -108,7 +68,7 @@ const fillCard = (data, district) => {
   })
 }
 
-//display two rows in a div
+//fill slide on double row
 const doubleCard = (data, district) => {
   let arr = []
 
@@ -138,7 +98,8 @@ const doubleCard = (data, district) => {
   return arr
 }
 
-const fillSlide = (data, district, classname) => {
+//prep slides to scroll
+const fillSlide = (data) => {
   let arr = []
   let arr_ = []
 
